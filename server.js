d7fb3db6319fbffd3c6245907368f8d00633522e
John Meikel Weill <jweill97@johns-mbp-2.home>
2020-10-31 23:16:13 -0400

    added mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // budget API
 const express = require('express');
+const 
 const cors = require('cors');
 const app = express();
 const port = 3000;
@@ -48,3 +49,63 @@ app.get('/budget', (req, res) => {
 app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`)
 });
+
+let url = 'mongodb://localhost:3000/budget';
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log("Connected to the database")
+    // list all entries
+   nameModel.find({})
+   .then((data)=>{
+       console.log(data)
+    })
+    .catch((connectionError) =>{
+       console.log(connectionError)
+    })
+})
+.catch((connectionError) => {
+   console.log(connectionError)
+})
+fetch
+nameModel.find({id: 2})
+    .then((data)=>{
+        console.log(data)
+    })
+    .catch((connectionError) =>{
+        console.log(connectionError)
+    })
+    nameModel.findById("5f9c9e28b035ae579b25e88a")
+    .then((data)=>{
+        console.log(data)
+        mongoose.connection.close()
+    })
+    .catch((connectionError) =>{
+        console.log(connectionError)
+    })
+    let newData = new namesModel({id: 10, name: "Testing mongoose"});
+    namesModel.insertMany(newData)
+    .then((data)=>{
+        console.log(data)
+        mongoose.connection.close()
+    })
+    .catch((connectionError) =>{
+        console.log(connectionError)
+    })
+    let newData = {id: 11, name: "updated content"};
+    namesModel.update({id: 10}, newData)
+    .then((data)=>{
+        console.log(data)
+        mongoose.connection.close()
+    })
+    .catch((connectionError) =>{
+        console.log(connectionError)
+    })
+    namesModel.remove({id: 10})
+    .then((data)=>{
+        console.log(data)
+        mongoose.connection.close()
+    })
+    .catch((connectionError) =>{
+        console.log(connectionError)
+    })
